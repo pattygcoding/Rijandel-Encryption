f91c3dd671088fc4c436b42457ef5fd2b205f4c6
Patrick Goodwin <pattygsocials@gmail.com>
2024-08-26 14:26:53 -0400

    Create Rijandel.kt

---
+++ Kotlin/192bit/Rijandel.kt
@@ -0,0 +1,29 @@
+import java.security.SecureRandom
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
+fun aesEncrypt(plaintext: String, key: ByteArray): ByteArray {
+    val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+    val iv = ByteArray(16)
+    SecureRandom().nextBytes(iv)
+    val ivSpec = IvParameterSpec(iv)
+    val keySpec = SecretKeySpec(key, "AES")
+
+    cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec)
+
+    val ciphertext = cipher.doFinal(plaintext.toByteArray(Charsets.UTF_8))
+    return iv + ciphertext
+}
+
+fun main() {
+    val keyGen = KeyGenerator.getInstance("AES")
+    keyGen.init(192) // 192-bit key
+    val key = keyGen.generateKey().encoded
+
+    val plaintext = "This is a test message for AES-192 encryption!"
+    val ciphertext = aesEncrypt(plaintext, key)
+
+    println("Ciphertext is: ${ciphertext.joinToString("") { "%02x".format(it) }}")
+}
